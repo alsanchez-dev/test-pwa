058a64e4f5ac4191e0dfa4139a1f06e5a26927f6
Al <al@MacBook-Pro.local>
2021-01-05 20:33:58 -0600

    Updates

---
+++ precache-manifest.737d4bf9de403552a892179be24a3dd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55591704292b0dd00c9b64e70420856",
+    "url": "/alsanchez-dev/test-pwa/index.html"
+  },
+  {
+    "revision": "395f0e34f9559eecdd82",
+    "url": "/alsanchez-dev/test-pwa/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "39fe8a65e7ae9d5f0957",
+    "url": "/alsanchez-dev/test-pwa/static/css/main.94e8dc10.chunk.css"
+  },
+  {
+    "revision": "395f0e34f9559eecdd82",
+    "url": "/alsanchez-dev/test-pwa/static/js/2.417b55d0.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/alsanchez-dev/test-pwa/static/js/2.417b55d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fe8a65e7ae9d5f0957",
+    "url": "/alsanchez-dev/test-pwa/static/js/main.1b3664b4.chunk.js"
+  },
+  {
+    "revision": "2018a80632a756cb916b",
+    "url": "/alsanchez-dev/test-pwa/static/js/runtime-main.ec2a8b4d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/alsanchez-dev/test-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
